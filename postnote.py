ce4fada30c81ac1c64134015d66669c4e0dbbf92
Jeff Geraci <jeffg777@gmail.com>
2024-11-17 09:22:45 -0500

    Further progress on the tray menu.

--- postnote.py
+++ postnote.py
@@ -32,7 +32,33 @@ class PostNoteWindow(QtWidgets.QMainWindow):
 
   def createNoteMenu(self):
     self.trayIconMenu = QtWidgets.QMenu(self)
+
+    # Create Note Size menu (for creating new notes)
+    self.noteSizeMenu = QtWidgets.QMenu('New Note', self)
+    self.noteSizeMenu.addAction(self.ui.actionSmall)
+    self.noteSizeMenu.addAction(self.ui.actionMedium)
+    self.noteSizeMenu.addAction(self.ui.actionLarge)
+    self.noteSizeMenu.addAction(self.ui.actionExtra_Large)
+
+    self.trayIconMenu.addMenu(self.noteSizeMenu)
+
+    # Create note list menu
+    self.noteListMenu = QtWidgets.QMenu('Show Note', self)
+    self.trayIconMenu.addMenu(self.noteListMenu)
+
+    self.trayIconMenu.addAction(self.ui.actionExport_Notes)
+    self.trayIconMenu.addAction(self.ui.actionEdit_Topics)
+    self.trayIconMenu.addAction(self.ui.actionPreferences)
+
     self.trayIconMenu.addAction(self.ui.actionSave_All_Notes)
+    self.trayIconMenu.addAction(self.ui.actionHide_All_Notes)
+    self.trayIconMenu.addAction(self.ui.actionShow_All_Notes)
+
+    self.trayIconMenu.addSeparator()
+    self.trayIconMenu.addAction(self.ui.actionAbout_Qt)
+    self.trayIconMenu.addAction(self.ui.actionAbout_PostNote)
+
+    self.trayIconMenu.addSeparator()
     self.trayIconMenu.addAction(self.ui.actionExit_PostNote)
 
     self.trayIcon.setContextMenu(self.trayIconMenu)
@@ -41,9 +67,63 @@ class PostNoteWindow(QtWidgets.QMainWindow):
   # ************ SLOTS ************
 
   @QtCore.Slot()
-  def on_actionSave_All_Notes(self):
+  def on_actionSmall_triggered(self):
     # TODO: Implement
-    pass
+    print('Create small note')
+
+  @QtCore.Slot()
+  def on_actionMedium_triggered(self):
+    # TODO: Implement
+    print('Create medium note')
+
+  @QtCore.Slot()
+  def on_actionLarge_triggered(self):
+    # TODO: Implement
+    print('Create large note')
+
+  @QtCore.Slot()
+  def on_actionExtra_Large_triggered(self):
+    # TODO: Implement
+    print('Create extra large note')
+
+  @QtCore.Slot()
+  def on_actionExport_Notes_triggered(self):
+    # TODO: Implement
+    print('Export notes')
+
+  @QtCore.Slot()
+  def on_actionEdit_Topics_triggered(self):
+    # TODO: Implement
+    print('Edit topics')
+
+  @QtCore.Slot()
+  def on_actionPreferences_triggered(self):
+    # TODO: Implement
+    print('Preferences')
+
+  @QtCore.Slot()
+  def on_actionSave_All_Notes_triggered(self):
+    # TODO: Implement
+    print('Save all notes')
+
+  @QtCore.Slot()
+  def on_actionHide_All_Notes_triggered(self):
+    # TODO: Implement
+    print('Hide all notes')
+
+  @QtCore.Slot()
+  def on_actionShow_All_Notes_triggered(self):
+    # TODO: Implement
+    print('Show all notes')
+
+  @QtCore.Slot()
+  def on_actionAbout_Qt_triggered(self):
+    QtWidgets.QMessageBox.aboutQt(None)
+
+  @QtCore.Slot()
+  def on_actionAbout_PostNote_triggered(self):
+    # TODO: Implement
+    print('About PostNote')
 
   @QtCore.Slot()
   def on_actionExit_PostNote_triggered(self):
