e0cb90ca822696327418132725109d1481079eee
Jeff Geraci <jeffg777@gmail.com>
2024-12-03 22:04:32 -0500

    Hook up all the buttons in the edit toolbar.  Still need to deal with the focus problem.

--- text_edit.py
+++ text_edit.py
@@ -76,41 +76,26 @@ class TextEdit(QtWidgets.QTextEdit):
     # TODO: Implement
     print('updateFontControls')
 
-  def onBoldTriggered(self):
-    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
-    tempCharFormat = QtGui.QTextCharFormat()
-
-    if selectionFormat.fontWeight() != QtGui.QFont.Weight.Bold:
-      tempCharFormat.setFontWeight(QtGui.QFont.Weight.Bold)
-    else:
-      tempCharFormat.setFontWeight(QtGui.QFont.Weight.Normal)
-
-    selectionCursor.mergeCharFormat(tempCharFormat)
-
-    self.setTextCursor(selectionCursor)
-
-  def onItalicTriggered(self):
-    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
-    tempCharFormat = QtGui.QTextCharFormat()
-
-    tempCharFormat.setFontItalic(not selectionFormat.fontItalic())
-    selectionCursor.mergeCharFormat(tempCharFormat)
-
-    self.setTextCursor(selectionCursor)
-
-  def onUnderlineTriggered(self):
-    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
-    tempCharFormat = QtGui.QTextCharFormat()
-
-    tempCharFormat.setFontUnderline(not selectionFormat.fontUnderline())
-    selectionCursor.mergeCharFormat(tempCharFormat)
-
-    self.setTextCursor(selectionCursor)
-
   def getCursorAndSelectionFormat(self) -> tuple[QtGui.QTextCursor, QtGui.QTextCharFormat]:
     selectionCursor = self.textCursor()
     selectionFormat = selectionCursor.charFormat()
     return (selectionCursor, selectionFormat)
+
+  def mergeCharFormat(self, tempCharFormat: QtGui.QTextCharFormat, selectionCursor: QtGui.QTextCursor):
+    selectionCursor.mergeCharFormat(tempCharFormat)
+    self.setTextCursor(selectionCursor)
+
+  def getCursorAndBlockFormat(self) -> tuple[QtGui.QTextCursor, QtGui.QTextBlockFormat]:
+    selectionCursor = self.textCursor()
+    selectionFormat = selectionCursor.blockFormat()
+    return (selectionCursor, selectionFormat)
+
+  def setBlockFormat(self, selectionCursor: QtGui.QTextCursor, selectionFormat: QtGui.QTextBlockFormat):
+    selectionCursor.setBlockFormat(selectionFormat)
+    self.setTextCursor(selectionCursor)
+
+  def getCursorAndListFormat(self) -> tuple[QtGui.QTextCursor, QtGui.QTextListFormat]:
+    return (self.textCursor(), QtGui.QTextListFormat())
 
   def onEditToolbarTriggered(self):
     self.editToolbar = EditToolbar(self.parent())
@@ -119,6 +104,16 @@ class TextEdit(QtWidgets.QTextEdit):
     self.editToolbar.toolbarClosing.connect(self.onToolbarClosing)
     self.editToolbar.fontSizeChanged.connect(self.onFontSizeChanged)
     self.editToolbar.fontFamilyChanged.connect(self.onFontFamilyChanged)
+    self.editToolbar.leftAlignTriggered.connect(self.onLeftAlignTriggered)
+    self.editToolbar.centerAlignTriggered.connect(self.onCenterAlignTriggered)
+    self.editToolbar.rightAlignTriggered.connect(self.onRightAlignTriggered)
+    self.editToolbar.boldTriggered.connect(self.onBoldTriggered)
+    self.editToolbar.italicTriggered.connect(self.onItalicTriggered)
+    self.editToolbar.underlineTriggered.connect(self.onUnderlineTriggered)
+    self.editToolbar.horizontalLineTriggered.connect(self.onHorizontalLineTriggered)
+    self.editToolbar.bulletListTriggered.connect(self.onBulletListTriggered)
+    self.editToolbar.numberListTriggered.connect(self.onNumberListTriggered)
+    self.editToolbar.tableTriggered.connect(self.onTableTriggered)
 
   def onToDefaultFontTriggered(self):
     if self.editToolbar is None:
@@ -158,15 +153,85 @@ class TextEdit(QtWidgets.QTextEdit):
     tempCharFormat = QtGui.QTextCharFormat()
 
     tempCharFormat.setFontPointSize(fontSize)
-    selectionCursor.mergeCharFormat(tempCharFormat)
-
-    self.setTextCursor(selectionCursor)
+    self.mergeCharFormat(tempCharFormat, selectionCursor)
 
   def onFontFamilyChanged(self, fontFamily: str):
     selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
     tempCharFormat = QtGui.QTextCharFormat()
 
     tempCharFormat.setFontFamily(fontFamily)
-    selectionCursor.mergeCharFormat(tempCharFormat)
+    self.mergeCharFormat(tempCharFormat, selectionCursor)
 
-    self.setTextCursor(selectionCursor)
+  def onLeftAlignTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndBlockFormat()
+    selectionFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
+    self.setBlockFormat(selectionCursor, selectionFormat)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onCenterAlignTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndBlockFormat()
+    selectionFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+    self.setBlockFormat(selectionCursor, selectionFormat)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onRightAlignTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndBlockFormat()
+    selectionFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+    self.setBlockFormat(selectionCursor, selectionFormat)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onBoldTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
+    tempCharFormat = QtGui.QTextCharFormat()
+
+    if selectionFormat.fontWeight() != QtGui.QFont.Weight.Bold:
+      tempCharFormat.setFontWeight(QtGui.QFont.Weight.Bold)
+    else:
+      tempCharFormat.setFontWeight(QtGui.QFont.Weight.Normal)
+
+    self.mergeCharFormat(tempCharFormat, selectionCursor)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onItalicTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
+    tempCharFormat = QtGui.QTextCharFormat()
+
+    tempCharFormat.setFontItalic(not selectionFormat.fontItalic())
+    self.mergeCharFormat(tempCharFormat, selectionCursor)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onUnderlineTriggered(self):
+    selectionCursor, selectionFormat = self.getCursorAndSelectionFormat()
+    tempCharFormat = QtGui.QTextCharFormat()
+
+    tempCharFormat.setFontUnderline(not selectionFormat.fontUnderline())
+    self.mergeCharFormat(tempCharFormat, selectionCursor)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onHorizontalLineTriggered(self):
+    self.insertHtml('<hr />')
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onBulletListTriggered(self):
+    self.createList(QtGui.QTextListFormat.Style.ListDisc)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+    self.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+    QtCore.QTimer.singleShot(100, self.setFocusToParent)
+
+  def setFocusToParent(self):
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def onNumberListTriggered(self):
+    self.createList(QtGui.QTextListFormat.Style.ListDecimal)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
+
+  def createList(self, style: QtGui.QTextListFormat.Style):
+    selectionCursor, newListFormat = self.getCursorAndListFormat()
+    newListFormat.setIndent(1)
+    newListFormat.setStyle(style)
+    selectionCursor.createList(newListFormat)
+
+  def onTableTriggered(self, numRows: int, numColumns: int):
+    selectionCursor = self.textCursor()
+    selectionCursor.insertTable(numRows, numColumns)
+    self.theParent.setFocus(QtCore.Qt.FocusReason.MouseFocusReason)
